0cb609ae924b67f7acab2d90dc1a2d8cc590cd09
Gagan Kumar <31625778+sgagankumar@users.noreply.github.com>
2020-03-23 20:23:01 +0530

    Add files via upload

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=D:\Programming materials\Flutter"
+export "FLUTTER_APPLICATION_PATH=D:\Projects\Flutter trial\startup_namer"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=D:\Programming materials\Flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
